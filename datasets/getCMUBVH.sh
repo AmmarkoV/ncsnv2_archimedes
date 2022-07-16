da5b13776a106c4c1500502a07beca8334b8665a
Ammar Qammaz <ammarkov@gmail.com>
2022-07-16 23:47:08 +0300

    ...

--- datasets/getCMUBVH.sh
+++ datasets/getCMUBVH.sh
@@ -4,17 +4,19 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$DIR"
 
 cd ..
+mkdir exp
+cd exp
 mkdir datasets
 cd datasets
 
 
-if [ -f CMUBVH ]
+if [ -f cmubvh ]
 then
 echo "CMU BVH datasets appear to have been downloaded.."
 else
   wget http://ammar.gr/datasets/archimedes.zip
   unzip archimedes.zip
-  mv dataset CMUBVH
+  mv dataset cmubvh
   cd "$DIR" 
 fi
 
