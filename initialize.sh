b77f0142920aa08734275bd6c63112408fb8b5a9
Ammar Qammaz <ammarkov@ics.forth.gr>
2022-08-03 16:01:32 +0300

    updated initialize

--- initialize.sh
+++ initialize.sh
@@ -12,11 +12,16 @@ python3 -m venv ncsnv2Env
 source ncsnv2Env/bin/activate
 python3 -m pip install -r requirements.txt
 
-
+mkdir debug
 mkdir -p exp/datasets
 cd exp/datasets/
-git clone https://github.com/fyu/lsun
-cd lsun
-python3 download.py
+#git clone https://github.com/fyu/lsun
+#cd lsun
+#python3 download.py
+
+cd $DIR
+cd datasets
+./getCMUBVH.sh
+
 
 exit 0
