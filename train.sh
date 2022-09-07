cf49a9af9145d52872ed3aec6f8ffae654c4a517
Ammar Qammaz <ammarkov@gmail.com>
2022-09-07 11:58:48 +0300

    add a training file..

---
+++ train.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "$DIR"
+
+#CUDA_VISIBLE_DEVICES="0"
+
+if (( $#<2 ))
+then 
+ echo "Please provide arguments first argument is configuration ,  second is experiment name/label "
+ exit 1
+else
+ CONFIGURATION=$1
+ EXPERIMENT=$2
+fi
+
+PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python python main.py --config $CONFIGURATION --doc $EXPERIMENT
+
+cd "exp/logs"
+scp -P 2222 -R $EXPERIMENT/ ammar@ammar.gr:/home/ammar/public_html/ncsnv2_archimedes/exp/logs/
+echo "scp -P 2222 -R $EXPERIMENT/ ammar@ammar.gr:/home/ammar/public_html/ncsnv2_archimedes/exp/logs/"
+
+exit 0
